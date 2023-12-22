83ead04620c03bd6ab0ea613bb4e4838aa5d8c53
seo <seo@seoui-noteubug.local>
2023-12-22 15:08:40 +0900

    02버전 푸쉬

--- dhere/src/main/java/com/springstudy/dhere/domain/Story.java
+++ dhere/src/main/java/com/springstudy/dhere/domain/Story.java
@@ -7,7 +7,10 @@ public class Story {
 	
 	private int storyNo;
 	private String title;
-	private String content;
+	private String content1;
+	private String content2;
+	private String content3;
+	private String content4;
 	private String email;
 	private Timestamp regDate;
 	private int categoryNo;
@@ -21,12 +24,15 @@ public class Story {
 	
 	public Story() {}
 	
-	public Story(int storyNo, String title, String content, String email, Timestamp regDate, int categoryNo, 
+	public Story(int storyNo, String title, String content1, String content2, String content3, String content4, String email, Timestamp regDate, int categoryNo, 
 			int readCount, int thank, String nickname, int imageNo, String fileName) {
 		
 	    this.storyNo = storyNo;
 	    this.title = title;
-	    this.content = content;
+	    this.content1 = content1;
+	    this.content2 = content2;
+	    this.content3 = content3;
+	    this.content4 = content4;
 	    this.email = email;
 	    this.regDate = regDate;
 	    this.categoryNo = categoryNo;
@@ -53,12 +59,36 @@ public class Story {
 		this.title = title;
 	}
 
-	public String getContent() {
-		return content;
+	public String getContent1() {
+		return content1;
 	}
 
-	public void setContent(String content) {
-		this.content = content;
+	public void setContent1(String content1) {
+		this.content1 = content1;
+	}
+	
+	public String getContent2() {
+		return content2;
+	}
+
+	public void setContent2(String content2) {
+		this.content2 = content2;
+	}
+	
+	public String getContent3() {
+		return content3;
+	}
+
+	public void setContent3(String content3) {
+		this.content3 = content3;
+	}
+	
+	public String getContent4() {
+		return content4;
+	}
+
+	public void setContent4(String content4) {
+		this.content4 = content4;
 	}
 
 	public String getEmail() {
